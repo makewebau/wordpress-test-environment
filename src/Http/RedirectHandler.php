0d494120dfe2fc324a829a6a82122cae97f3b9bc
Andrew Feeney <webspannerdev@gmail.com>
2018-07-07 08:46:19 +1000

    Move functionality to RedirectHandler class

--- src/Http/RedirectHandler.php
+++ src/Http/RedirectHandler.php
@@ -1,14 +1,13 @@
 <?php
 
-namespace MakeWeb\WordpressTestEnvironment;
+namespace MakeWeb\WordpressTestEnvironment\Http;
 
-class Http
+class RedirectHandler
 {
     public function handleRedirect(...$args)
     {
         if ($args[0] == 'http:/wp-admin/install.php') {
             throw new \Exception('Wordpress is not installed');
         }
-        var_dump($args);
     }
 }
